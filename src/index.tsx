ea9c3d998334a98791674e519d5675ad4901ec5a
Nico Schett <nicoschett@icloud.com>
2021-06-11 22:30:00 +0200

    Move cms.scss import

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {PersistGate} from 'redux-persist/integration/react'
 
+import './cms.scss'
 import {
   ConnectedPageType,
   CMSProvider,
