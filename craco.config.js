0c48f32cb826a4e4e3603e13f746767817ed4f67
Nico Schett <nicoschett@icloud.com>
2021-07-03 00:35:24 +0000

    Add license tag

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,11 @@
-/* craco.config.js */
+/**
+ * @license
+ * Copyright snek-at. All Rights Reserved.
+ *
+ * Use of this source code is governed by an EUPL-1.2 license that can be found
+ * in the LICENSE file at https://snek.at/license
+ */
+
 const path = require(`path`)
 
 module.exports = {
